b353aafe1c4fdb8b7933d471ffe5c80836b4fba7
Maarten Slagter <slagter.maarten@gmail.com>
2017-10-19 11:05:21 +0200

    Set panel size of ggplot object

--- R/plotting.R
+++ R/plotting.R
@@ -93,3 +93,47 @@ normalize_grob_heights <- function(plots, ...) {
                     function(x) { x$heights[6] <- max_h; return(x) })
   return(gtabs_c)
 }
+
+
+#' Fixate panel size of ggplot
+#'
+#' @param p ggplot object
+#' @param g ggplotGrob
+#' @param file file name to save result to. File is saved using \code{ggplot2::ggsave()}
+#' @param margin \code{grid::unit()} object indicating margin size 
+#' @param width \code{grid::unit()} object indicating panel width
+#' @param height \code{grid::unit()} object indicating panel height
+#' @return gtable object
+set_panel_size <- function(p=NULL, g=ggplotGrob(p), 
+                           file=NULL,
+                           margin = unit(1,'mm'),
+                           width=unit(4, 'cm'),
+                           height=unit(4, 'cm')) {
+  panels <- grep('panel', g$layout$name)
+  panel_index_w<- unique(g$layout$l[panels])
+  panel_index_h<- unique(g$layout$t[panels])
+  nw <- length(panel_index_w)
+  nh <- length(panel_index_h)
+
+	if (getRversion() < '3.3.0') {
+		 ## The following conversion is necessary
+		 ## because there is no `[<-`.unit method
+		 ## so promoting to unit.list allows standard list indexing
+		 g$widths <- grid:::unit.list(g$widths)
+		 g$heights <- grid:::unit.list(g$heights)
+		 g$widths[panel_index_w] <-  rep(list(width),  nw)
+		 g$heights[panel_index_h] <- rep(list(height), nh)
+	} else {
+		 g$widths[panel_index_w] <-  rep(width,  nw)
+		 g$heights[panel_index_h] <- rep(height, nh)
+	}
+
+  if(!is.null(file))
+    ggsave(file, g,
+           width = convertWidth(sum(g$widths) + margin,
+                                unitTo = 'in', valueOnly = TRUE),
+           height = convertHeight(sum(g$heights) + margin,
+                                  unitTo = 'in', valueOnly = TRUE))
+
+  invisible(g)
+}
