108ed4a9b545542ea561df8e2bb528ff2e8c9071
Maarten Slagter <slagter.maarten@gmail.com>
2019-10-14 13:04:26 +0200

    Added utilities for converting various color encodings

--- R/plotting.R
+++ R/plotting.R
@@ -485,6 +485,81 @@ lighten <- function(color_vec, factor = 1.4) {
 }
 
 
+#' Convert HSL coordinates to RGB coordinates
+#' 
+#' @return r/g/b in a vector 
+hsl_to_rgb <- function(h, s, l) {
+  h <- h / 360
+  r <- g <- b <- 0.0
+  if (s == 0) {
+    r <- g <- b <- l
+  } else {
+    hue_to_rgb <- function(p, q, t) {
+      if (t < 0) { t <- t + 1.0 }
+      if (t > 1) { t <- t - 1.0 }
+      if (t < 1/6) { return(p + (q - p) * 6.0 * t) }
+      if (t < 1/2) { return(q) }
+      if (t < 2/3) { return(p + ((q - p) * ((2/3) - t) * 6)) }
+      return(p)
+    }
+    q <- ifelse(l < 0.5, l * (1.0 + s), l + s - (l*s))
+    p <- 2.0 * l - q
+    r <- hue_to_rgb(p, q, h + 1/3)
+    g <- hue_to_rgb(p, q, h)
+    b <- hue_to_rgb(p, q, h - 1/3)
+  }
+  return(c('r' = r, 'g' = g, 'b' = b))
+}
+
+
+#' Convert RGB coordinates to HSL coordinates
+#' 
+#' h = 0-360 deg, s = 0.0 - 1 (0-100%), l = 0.0 - 1 (0-100%)
+#' 
+#' @return h/s/l in a vector 
+rgb_to_hsl <- function(r, g, b) {
+  val_max <- max(c(r, g, b))
+  val_min <- min(c(r, g, b))
+  h <- s <- l <- (val_max + val_min) / 2
+  if (val_max == val_min){
+    h <- s <- 0
+  } else {
+    d <- val_max - val_min
+    s <- ifelse(l > 0.5, d / (2 - val_max - val_min), d / (val_max + val_min))
+    if (val_max == r) { h <- (g - b) / d + (ifelse(g < b, 6, 0)) }
+    if (val_max == g) { h <- (b - r) / d/ + 2 }
+    if (val_max == b) { h <- (r - g) / d + 4 }
+    h <- (h / 6) * 360
+  }
+  return(c('h' = h, 's' = s , 'l' = l))
+}
+
+
+#' Convert HSL coordinates to hexadecimal color code
+#' 
+#' @return HEX color code
+hsl_to_hex <- function(h, s, l) {
+  rgb_coords <- do.call(hsl_to_rgb, list('h' = h, 's' = s, 'l' = l))
+  do.call(rgb_to_hex, as.list(rgb_coords))
+}
+
+
+#' convert rgb coordinates to hexadecimal color code
+#' 
+#' @return hex color code
+rgb_to_hex <- function(r, g, b, nf = 255) {
+  do.call(rgb, as.list(c('red' = r, 'green' = g, 'blue' = b) / nf))
+}
+
+
+#' Convert hex code to hexadecimal color code
+#' 
+#' @return RGB coordinates (scale: [0, 255])
+hex_to_rgb <- function(hex) {
+  as.vector(col2rgb(hex))
+}
+
+
 #' Create vector of colors from color palette
 #'
 #' @param n Amount of primary colors to interpolate from palette
@@ -574,6 +649,10 @@ gen_color_palette <- function(name = 'Set1', n = 30L, prims = NA) {
 
   ramp <- colorRampPalette(pal, alpha = TRUE)
   return(ramp)
+}
+
+
+color_palette <- function(cols) {
 }
 
 
